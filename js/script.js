2132736f1f4a9df84ccca931b89d7b814367562c
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-03-22 18:44:15 +0000

    uu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Kenneth
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculatePermeterAndAreaOfRectangle() {
+  // input
+  const lengthOfRectangle = parseInt(document.getElementById('length-of-rectangle').value)
+  const heightOfRectangle = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const areaOfRectangle = lengthOfRectangle * heightOfRectangle
+  const perimeterOfRectangle = (lengthOfRectangle + heightOfRectangle) * 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfRectangle + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeterOfRectangle + ' cm'
+}
